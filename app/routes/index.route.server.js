5a28b5793f002997c7abb7f722729c32af2f4eb6
PacificZenA <104084600+PacificZenA@users.noreply.github.com>
2022-09-25 09:55:53 -0400

    Nav bar

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,12 +1,22 @@
 //Index routes
 import { Router } from "express";
-import { helloWorld } from "../controllers/index.controller.server.js";
+import {
+  displayAboutPage,
+  displayContactPage,
+  DisplayHomePage,
+  displayProjectsPage,
+  displayServicesPage,
+} from "../controllers/index.controller.server.js";
 
 //Instanciating the router
 const router = Router();
 
 //Add middleware to connect application
-router.get("/", helloWorld);
-router.get("/hello", helloWorld);
+router.get("/", DisplayHomePage);
+router.get("/home", DisplayHomePage);
+router.get("/about", displayAboutPage);
+router.get("/projects", displayProjectsPage);
+router.get("/services", displayServicesPage);
+router.get("/contact", displayContactPage);
 
 export default router;
